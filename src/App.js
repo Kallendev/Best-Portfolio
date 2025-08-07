53ef0d32d8dec83e7b27aedc71eed4a008ee49e5
kallendev <kallemugambi@gmail.com>
2025-08-07 11:45:30 +0300

    updated routes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,17 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import React from 'react';
 
-
 function App() {
   return (
-     <BrowserRouter basename="/Best-Portfolio">
-      <Router>
+    <Router basename="/Best-Portfolio">
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/about" element={<Layout><About /></Layout>} />
         <Route path="/services" element={<Layout><Services /></Layout>} />
         <Route path="/portfolio" element={<Layout><Portfolio /></Layout>} />
         <Route path="/contact" element={<Layout><Contact /></Layout>} />
-  
       </Routes>
     </Router>
-    </BrowserRouter>
   );
 }
 
